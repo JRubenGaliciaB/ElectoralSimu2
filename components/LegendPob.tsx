7b80353fd1e475c548b3750bb3c116a3cbdb7237
JRubenGaliciaB <jrubengaliciab@gmail.com>
2025-12-06 09:04:55 -0600

    color

--- components/LegendPob.tsx
+++ components/LegendPob.tsx
@@ -12,7 +12,7 @@ const LegendPob: React.FC<LegendPobProps> = ({ title, colorStops, minLabel, maxL
 
   return (
     <div className="absolute bottom-7 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[450] bg-white/20 backdrop-blur-sm p-1 rounded-lg shadow-xl border border-gray-200 w-64">
-      <h3 className="text-sm font-bold mb-2 text-gray-800 text-center">{title}</h3>
+      <h3 className="text-sm font-bold mb-2 text-white text-center">{title}</h3>
       <div className="w-full h-5 rounded-md border border-gray-300" style={{ background: gradient }}></div>
       <div className="flex justify-between mt-1 text-xs text-gray-700 font-medium">
         <span>{minLabel}</span>
